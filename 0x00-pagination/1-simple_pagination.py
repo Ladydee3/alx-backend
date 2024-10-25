2b905319fb9bc367c14b462c2ae82e13c2ccf5b8
Ladydee3 <delisiletwala62@gmail.com>
2024-10-25 10:47:25 +0000

    1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -21,28 +21,26 @@ class Server:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
-            self.__dataset = dataset[1:]  # Skip the header row
+            self.__dataset = dataset[1:]
 
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """ Finds the correct indexes to paginate dataset.
         """
-        assert type(page) == int, "Page must be an integer"
-        assert type(page_size) == int, "Page size must be an integer"
-        assert page > 0, "Page must be greater than 0"
-        assert page_size > 0, "Page size must be greater than 0"
-        
+        assert type(page) == int
+        assert type(page_size) == int
+        assert page > 0
+        assert page_size > 0
+        csv_size = len(self.dataset())
         start, end = index_range(page, page_size)
-        dataset = self.dataset()
-        if start >= len(dataset):
-            return []  # Return an empty list if the start index is out of bounds
-
-        return dataset[start:end]
+        end = min(end, csv_size)
+        if start >= csv_size:
+            return []
+        return self.dataset()[start:end]
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """ Returns a tuple containing a start and end index.
     """
     return ((page - 1) * page_size, page * page_size)
-
