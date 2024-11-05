9ca9ce31931be79fabf6f98355b75f4874d13c03
Ladydee3 <delisiletwal62@gmail.com>
2024-11-05 17:49:09 +0000

    5-app.py

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python3
-"""Task 5"""
-from flask import Flask, g, render_template, request
+
+"""
+5. Basic Flask app
+"""
+
+from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
 app = Flask(__name__)
 babel = Babel(app)
+
+
+class Config:
+    """
+    Config class.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
@@ -13,44 +30,41 @@ users = {
 }
 
 
-class Config(object):
-    """simple configuration"""
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
-
-app.config.from_object(Config)
-
-
-def get_user():
-    """get user from header"""
-    id = request.args.get('login_as')
+def get_user(login_as):
+    """
+    get_user.
+    """
     try:
-        return users.get(int(id))
+        return users.get(int(login_as))
     except Exception:
-        return None
+        return
 
 
 @app.before_request
 def before_request():
-    """Before request used to stash user"""
-    g.user = get_user()
+    """
+    before_request
+    """
+    g.user = get_user(request.args.get("login_as"))
 
 
 @babel.localeselector
 def get_locale():
-    """locale selector determining lang use for template"""
-    loc= request.args.get('locale')
-    if loc and loc in app.config['LANGUAGES']:
-        return loc
-    return request.accept_languages.best_match(Config.LANGUAGES)
+    """
+    get_locale.
+    """
+    locale = request.args.get("locale")
+    if locale:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route("/", methods=['GET'])
-def index():
-    """index rotue"""
-    return render_template("6-index.html")
+@app.route('/', methods=["GET"], strict_slashes=False)
+def hello():
+    """
+    hello.
+    """
+    return render_template('5-index.html')
 
 
 if __name__ == "__main__":
